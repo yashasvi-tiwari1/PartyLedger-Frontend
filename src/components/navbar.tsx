957ec200719ac64930a92ec6db626c1b7e672285
yashasvitiwari <yashasvi.tiwari.4648@gmail.com>
2023-10-17 09:03:34 +0545

    Dashboard updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,6 @@
-import {
-  IconBell,
-  IconChevronDown,
-  IconSearch,
-  IconUserBolt,
-} from "@tabler/icons-react";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import Image from "next/image";
 
 export default function Navbar() {
   const [showData, setShowData] = React.useState<string | null>(null);
@@ -31,15 +26,23 @@ export default function Navbar() {
         <span className="text-2xl font-medium tracking-wide text-gray-700">
           Dashboard
         </span>
+        <div className=" font-semibold text-2xl ">Welcome Back!</div>
 
         <div className="flex items-center gap-6">
-          <div className="flex gap-4 items-center">
-            <div>
-              <IconUserBolt />
-            </div>
+          <div className="flex gap-4 items-center ">
             <div className="">
-              <p></p>
-              {/* <p className="-mt-1"> {userDetails.name}</p> */}
+              <span className="font-semibold text-lg ml-2">
+                Mr. Omkar Tiwari
+              </span>
+            </div>
+            <div className="rounded-full bg-gray-400 border-2 border-teal-500 ">
+              <Image
+                src="/assets/profile.png"
+                alt="profile"
+                height={150}
+                width={150}
+                className="w-16 -mt-2"
+              />
             </div>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,34 @@
+"use client";
 import Layout from "../components/layout";
 import { ReactElement } from "react";
 import {
   IconHomeDollar,
   IconSquareLetterC,
-  IconSquareLetterF,
-  IconSquareLetterM,
   IconUserSquare,
 } from "@tabler/icons-react";
 import { NextPageWithLayout } from "./_app";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 const Dashboard: NextPageWithLayout = () => {
+  const navigate = useRouter();
+
   return (
     <>
-      <div className="">
+      <div className="py-4">
         <div className="w-[980px] bg-dashboard p-6 rounded-xl">
           <span className="text-xl font-semibold "> Summary </span>
           <div className="flex justify-between mt-10 ">
             <div className="bg-orange-200 p-6 w-52 text-center rounded-lg">
-              <div className="flex justify-center">
-                <IconUserSquare className="w-9 h-9 text-orange-600 " />
-              </div>
-              <div className="font-semibold text-lg">3</div>
-              <div className="font-semibold text-lg text-gray-600">
-                Total Users
-              </div>
+              <Link href="/customer">
+                <div className="flex justify-center">
+                  <IconUserSquare className="w-9 h-9 text-orange-600 " />
+                </div>
+                <div className="font-semibold text-lg"></div>
+                <div className="font-semibold text-lg text-gray-600">
+                  Total Customers
+                </div>
+              </Link>
             </div>
             <div className="bg-green-200 flex flex-col justify-center w-52 text-center p-6 rounded-lg">
               <div className="flex justify-center">
@@ -30,7 +36,7 @@ const Dashboard: NextPageWithLayout = () => {
               </div>
               <div className="font-semibold text-lg">350</div>
               <div className="font-semibold text-lg text-gray-600">
-                Total Customers
+                Tota
               </div>
             </div>
             <div className="bg-purple-200 p-6 rounded-lg text-center w-52 ">
